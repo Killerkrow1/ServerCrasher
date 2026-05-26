4190088be3a499a3c987de0bf9e9f0b439276901
Killerkrow <cpf283@gmail.com>
2026-05-25 21:41:03 -0500

    Initial commit

---
+++ src/main/java/paradigm/killerkrow/servercrasher/Servercrasher.java
@@ -0,0 +1,10 @@
+package paradigm.killerkrow.servercrasher;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Servercrasher implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/paradigm/killerkrow/servercrasher/client/ServercrasherClient.java
@@ -0,0 +1,10 @@
+package paradigm.killerkrow.servercrasher.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ServercrasherClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
